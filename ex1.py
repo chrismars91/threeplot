5a57c0977f64c6165a4d14626377da43ffadb4ba
Chris Bolig <chrisbolig@Chriss-MacBook-Pro.local>
2024-08-14 20:05:49 -0400

    dark_theme ex1

--- ex1.py
+++ ex1.py
@@ -10,4 +10,4 @@ x2, y2 = np.meshgrid(np.arange(-5, 5, 0.25), np.arange(-5, 5, 0.25))
 z2 = 2 * np.sin(np.sqrt(x2 ** 2 + y2 ** 2))
 
 #  y is up in threejs so if z is up in python x,y,z --> x,z,-y
-plot3D([x2, z2, -y2, x1, z1 + 3.5, -y1], title="3D Plot", applyCmaps=[False, False], theme="light_theme")
+plot3D([x2, z2, -y2, x1, z1 + 3.5, -y1], title="3D Plot", applyCmaps=[True, False])
